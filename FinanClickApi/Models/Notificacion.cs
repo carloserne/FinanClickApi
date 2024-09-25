0a2dc141eb320ab405e55e5a41998d5c89782526
carloserne <carene2003@gmail.com>
2024-09-25 17:03:46 -0600

    add: Modelo Notificacion y conexion con la BD

---
+++ FinanClickApi/Models/Notificacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinanClickApi.Models;
+
+public partial class Notificacion
+{
+    public int Id { get; set; }
+
+    public string Mensaje { get; set; } = null!;
+
+    public DateTime? FechaCreacion { get; set; }
+
+    public bool? Leido { get; set; }
+
+    public int IdRol { get; set; }
+
+    public virtual Rol IdRolNavigation { get; set; } = null!;
+}
