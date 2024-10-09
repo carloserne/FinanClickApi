f2a93806ca526181f8d027aac965b89c04bf684e
carloserne <carene2003@gmail.com>
2024-10-09 00:03:57 -0600

    add: quejas y sugerencias controller apis

---
+++ FinanClickApi/Controllers/QuejaSugerenciumController.cs
@@ -0,0 +1,112 @@
+﻿using FinanClickApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace FinanClickApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class QuejaSugerenciumController : ControllerBase
+    {
+        private readonly FinanclickDbContext _baseDatos;
+
+        public QuejaSugerenciumController(FinanclickDbContext baseDatos)
+        {
+            _baseDatos = baseDatos;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<QuejaSugerencium>>> GetQuejasSugerencias()
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _baseDatos.Usuarios.FindAsync(int.Parse(currentUserId));
+
+            return await _baseDatos.QuejaSugerencia
+                .Where(q => q.IdEmpresa == user.IdEmpresa)
+                .ToListAsync();
+        }
+
+        // GET: api/QuejaSugerencium/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<QuejaSugerencium>> GetQuejaSugerencium(int id)
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _baseDatos.Usuarios.FindAsync(int.Parse(currentUserId));
+
+            var quejaSugerencium = await _baseDatos.QuejaSugerencia
+                .Where(q => q.IdEmpresa == user.IdEmpresa && q.IdQuejaSugerencia == id)
+                .FirstOrDefaultAsync();
+
+            if (quejaSugerencium == null)
+            {
+                return NotFound();
+            }
+
+            return quejaSugerencium;
+        }
+
+        // POST: api/QuejaSugerencium
+        [HttpPost]
+        public async Task<ActionResult<QuejaSugerencium>> PostQuejaSugerencium(QuejaSugerencium quejaSugerencium)
+        {
+            _baseDatos.QuejaSugerencia.Add(quejaSugerencium);
+            await _baseDatos.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetQuejaSugerencium), new { id = quejaSugerencium.IdQuejaSugerencia }, quejaSugerencium);
+        }
+
+        // PUT: api/QuejaSugerencium/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuejaSugerencium(int id, QuejaSugerencium quejaSugerencium)
+        {
+            if (id != quejaSugerencium.IdQuejaSugerencia)
+            {
+                return BadRequest();
+            }
+
+            _baseDatos.Entry(quejaSugerencium).State = EntityState.Modified;
+
+            try
+            {
+                await _baseDatos.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuejaSugerenciumExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/QuejaSugerencium/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuejaSugerencium(int id)
+        {
+            var quejaSugerencium = await _baseDatos.QuejaSugerencia.FindAsync(id);
+            if (quejaSugerencium == null)
+            {
+                return NotFound();
+            }
+
+            _baseDatos.QuejaSugerencia.Remove(quejaSugerencium);
+            await _baseDatos.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuejaSugerenciumExists(int id)
+        {
+            return _baseDatos.QuejaSugerencia.Any(q => q.IdQuejaSugerencia == id);
+        }
+    }
+}
