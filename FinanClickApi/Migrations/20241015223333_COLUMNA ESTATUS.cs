61a1f35c57f29b071efd195ecd9c8b4ecb489f91
carloserne <carene2003@gmail.com>
2024-10-15 16:34:54 -0600

    migracion de la columna de estatus

---
+++ FinanClickApi/Migrations/20241015223333_COLUMNA ESTATUS.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanClickApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class COLUMNAESTATUS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Estado",
+                table: "QuejaSugerencia",
+                newName: "Estatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Estatus",
+                table: "QuejaSugerencia",
+                newName: "Estado");
+        }
+    }
+}
